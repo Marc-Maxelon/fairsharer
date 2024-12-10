7daf49122791289ed1ea46a9146b520f1dc7ecd6
Marc-Maxelon <marc.maxelon@study.hs-duesseldorf.de>
2024-12-10 22:21:55 +0100

    Change import

--- src/test_fair_sharer.py
+++ src/test_fair_sharer.py
@@ -1,9 +1,9 @@
-import fair_sharer
+from src.fair_sharer import fair_sharer as fs
 
-test_1 = fair_sharer([0, 1000, 800, 0], 1)
+test_1 = fs([0, 1000, 800, 0], 1)
 print(f"{test_1}")
 
 print("\n")
 
-test_2 = fair_sharer([0, 1000, 800, 0], 2)
+test_2 = fs([0, 1000, 800, 0], 2)
 print(f"{test_2}")
