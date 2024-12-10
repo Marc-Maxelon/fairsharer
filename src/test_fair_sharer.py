90f28148c3f5d0ea0d4e015a87566a4b93cb516e
Marc-Maxelon <marc.maxelon@study.hs-duesseldorf.de>
2024-12-10 21:25:31 +0100

    Add test_fair_sharer.py

---
+++ src/test_fair_sharer.py
@@ -0,0 +1,9 @@
+from fair_sharer import fair_sharer
+
+test_1 = fair_sharer([0, 1000, 800, 0], 1)
+print(f"{test_1}")
+
+print("\n")
+
+test_2 = fair_sharer([0, 1000, 800, 0], 2)
+print(f"{test_2}")
