38badd472c1fee5c6f1eff404c4ff6a8f22d7586
Marc-Maxelon <marc.maxelon@study.hs-duesseldorf.de>
2024-12-10 22:32:58 +0100

    Define expactancies

--- src/test_fair_sharer.py
+++ src/test_fair_sharer.py
@@ -1,18 +1,13 @@
 from fair_sharer import fair_sharer as fs
 
-def run_fair_sharer_tests():
+def test_fair_sharer_case_1():
     test_1 = fs([0, 1000, 800, 0], 1)
     print(f"Test 1: {test_1}")
+    expected_1 = [100.0, 800.0, 900.0, 0.0]
+    assert test_1 == expected_1, f"Test 1 fehlgeschlagen: {test_1}"
 
-    print("\n")
-
+def test_fair_sharer_case_2():
     test_2 = fs([0, 1000, 800, 0], 2)
     print(f"Test 2: {test_2}")
-
-    return test_1, test_2
-
-def test_fair_sharer():
-    test_1, test_2 = run_fair_sharer_tests()
-
-    assert test_1 == [100, 1000, 800, 0], f"Test 1 fehlgeschlagen: {test_1}"
-    assert test_2 == [100, 1000, 800, 0], f"Test 2 fehlgeschlagen: {test_2}"
+    expected_2 = [100.0, 890.0, 720.0, 90.0]
+    assert test_2 == expected_2, f"Test 2 fehlgeschlagen: {test_2}"
